3af21707788a5ea6494e13d67c863aa14005720b
lyena8958 <89381986+lyena8958@users.noreply.github.com>
2021-09-25 23:18:04 +0900

    9/25 Control(UC) - Comment CUD Parameter 추가

--- src/controller/userComment_Ctrl/C_DeleteComment_Action.java
+++ src/controller/userComment_Ctrl/C_DeleteComment_Action.java
@@ -30,10 +30,9 @@ public class C_DeleteComment_Action implements Action{
 	    //DAO 수행
 	    // 댓글 삭제 완료
 	    if (commentDAO.DeleteDB(commentVO)) {
-	    	//selectOne.pdo 수행하기 위한, 데이터 set
-	    	request.setAttribute("pnum", request.getParameter("c_post"));
+	    	String parameter = "?pnum="+request.getParameter("c_post");
 	    	forward.setRedirect(false); // sendRedirect
-	    	forward.setPath("selectOne.pdo"); // post 컨트롤러에게 페이지 요청(ShowList(단일 게시물)페이지로 이동하기 위해)
+	    	forward.setPath("selectOne.pdo"+parameter); // post 컨트롤러에게 페이지 요청(ShowList(단일 게시물)페이지로 이동하기 위해)
 	    }
 	    // 반영 실패 -> 오류 수행
 	    else {
