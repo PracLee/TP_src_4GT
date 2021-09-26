ac586b2a212f72c67d4b402efbae622fb1e2b678
OHT <gus3578@naver.com>
2021-09-26 10:30:00 +0900

    LikeInfoDAO 오류 수정

--- src/model/likeInfo/LikeInfoDAO.java
+++ src/model/likeInfo/LikeInfoDAO.java
@@ -14,7 +14,7 @@ public class LikeInfoDAO {
    private static String sql_SELECT_ALL = "SELECT * FROM likeInfo"; // 추후 content, condition 나눠서 진행
    private static String sql_SELECT_ONE = "SELECT * FROM likeInfo WHERE l_user=? AND l_post=?";
    private static String sql_INSERT = "INSERT INTO likeInfo (l_user, l_post) VALUES(?, ?)";
-   private static String sql_DELETE = "DELETE FROM likeInfo WHERE l_user=? l_post=?";
+   private static String sql_DELETE = "DELETE FROM likeInfo WHERE l_user=? AND l_post=?";
    
    // private static String sql_UPDATE = "UPDATE likeInfo SET l_user=?, l_post=? WHERE l_post=?";
    // UPDATE 쓸 일 없을거같아서 일단 보류
