015be26fe72cdbf4ea1942b2f6403a03091465d4
PracLee <mrgoga@naver.com>
2021-10-06 14:49:34 +0900

    condition에 따른 검색결과

--- src/controller/post_ctrl/post/read/FindPost.java
+++ src/controller/post_ctrl/post/read/FindPost.java
@@ -25,46 +25,21 @@ public class FindPost implements Action{
 		String condition = (String)request.getParameter("condition");
 		// 각 항목 페이징 처리
 		String url="result.jsp";	
-		String tIndexx=request.getParameter("tIndex");
-		String cIndexx=request.getParameter("cIndex");
-		String wIndexx=request.getParameter("wIndex");
-		int titleIndex=6;
-		int contentIndex=6;
-		int writerIndex=6;
-		if(tIndexx!=null){
-			titleIndex=Integer.parseInt(tIndexx);
-		}
-		if(cIndexx!=null){
-			contentIndex=Integer.parseInt(cIndexx);
-		}
-		if(wIndexx!=null){
-			writerIndex=Integer.parseInt(wIndexx);
+		String indexx=request.getParameter("index");
+		int index=6;
+		if(indexx!=null){
+			index=Integer.parseInt(indexx);
 		}
 		//url 작업
-		url= url+ "?tIndex="+titleIndex;
-		url= url+ "&cIndex="+contentIndex;
-		url= url+ "&wIndex="+writerIndex;
-		request.setAttribute("tIndex", titleIndex);
-		request.setAttribute("cIndex", contentIndex);
-		request.setAttribute("wIndex", writerIndex);
+		url= url+ "?index="+index;
+		request.setAttribute("index", index);
 		// 제목검색 결과
-		ArrayList<PostVO> titleResult = PDAO.SearchPostTitle(find,titleIndex);
-		if(titleResult!=null) {
-			request.setAttribute("titleResult", titleResult);
-			System.out.println("titleResult"+ titleResult);			
+		ArrayList<PostVO> result = PDAO.SearchPostTitle(find,index);	// condition추가
+		if(result!=null) {
+			request.setAttribute("result", result);
+			System.out.println("result"+ result);			
 		}
-		// 내용검색 결과
-		ArrayList<PostVO> contentResult = PDAO.SearchPostContent(find,contentIndex);
-		if(contentResult!=null) {
-			request.setAttribute("contentResult", contentResult);
-			System.out.println("contentResult"+ contentResult);			
-		}
-		// 글쓴이 검색 결과
-		ArrayList<PostVO> writerResult = PDAO.SearchPostWriter(find,writerIndex);
-		if(writerResult!=null) {			
-			request.setAttribute("writerResult", writerResult);
-			System.out.println("writerResult"+ writerResult);
-		}
+		request.setAttribute("condition", condition);
 		request.setAttribute("word", word);
 		action.setPath("result.jsp");
 		action.setRedirect(false);
