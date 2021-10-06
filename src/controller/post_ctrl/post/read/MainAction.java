1a659a10d8169e03b0e8ab9601aa36ab8c64b51c
PracLee <mrgoga@naver.com>
2021-10-06 22:08:28 +0900

    PagingController 완성

--- src/controller/post_ctrl/post/read/MainAction.java
+++ src/controller/post_ctrl/post/read/MainAction.java
@@ -2,7 +2,6 @@ package controller.post_ctrl.post.read;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -10,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import controller.Action;
 import controller.ActionForward;
+import model.post.Paging;
 import model.post.PostDAO;
 import model.post.PostVO;
 
@@ -44,16 +44,22 @@ public class MainAction implements Action{
 			// System.out.println("index : "+index);
 			commentsCnt.add(index, (commentsCnt.get(index) + 1)); // commentsCnt index = postnum - 1
 		}*/	// 댓글수 로직 -> 0927 model column 추가로 삭제
-
-		int maxIndex = 1;
-		int listCnt = 6;
-		Paginator page = new Paginator(5, listCnt, datas.size());
-		// 인덱스 조정
-		if(datas != null) {
-			maxIndex = page.getTotalLastPageNum();
-		}
-		Map<String,Object> pageInfo = page.getFixedBlock(index);
 		
+		// 한페이지에 들어가는 게시물 수
+		int pagingSize = 6;
+		// 생성자 순서대로 int pageSize(한페이지 게시물 수), int thisPageNum(현재 페이지 번호), int totalPostCnt(총 포스트 갯수)
+		Paging paging = new Paging(pagingSize,index,datas.size());
+		paging.makePaging();
+		// 페이징 for문으로 표기 할 수 있도록 하기
+		ArrayList<Integer> pagingIndex = new ArrayList<Integer>();
+		int page = paging.getStartPageNum();
+		//					1							5
+		for(int i = paging.getStartPageNum();i<=paging.getEndPageNum();i++) {
+			pagingIndex.add(page);
+			page++;
+		}
+		
+		// 해당하는 페이지에 데이터만 주기
 		ArrayList<PostVO> slicedata = new ArrayList<PostVO>();
 		for(int i=(index-1)*6; i<index*6; i++) { // 현재 인덱스에 -1*6~현재인덱스*6 까지의 데이터만 넘겨주기
 			PostVO vo = datas.get(i);
@@ -63,8 +69,10 @@ public class MainAction implements Action{
 			}
 		}
 
-
-		request.setAttribute("maxIndex", maxIndex);
+		request.setAttribute("isLast", paging.isLast());
+		request.setAttribute("isFirst", paging.isFirst());
+		request.setAttribute("pagingIndex", pagingIndex);
+		request.setAttribute("index", index);
 		request.setAttribute("PostList", slicedata); // PostList로 SelectAll 데이터를 넘김
 		//System.out.println("mainActio 에서 보내주는 datas == "+datas);
 		// request.setAttribute("commentsCnt", commentsCnt); // 댓글 수 AL로 넘김 0번 인덱스에 1번 포스트의 댓글 갯수 담겨있음! 댓글수 로직 -> 0927 model column 추가로 삭제
