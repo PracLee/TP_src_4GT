2643e4eafc923cb9717df103806c3ae392b4346b
PracLee <mrgoga@naver.com>
2021-10-10 14:59:55 +0900

    데이터 슬라이스, new 태그 관련 캡슐화

--- src/controller/post_ctrl/post/read/PostAction.java
+++ src/controller/post_ctrl/post/read/PostAction.java
@@ -70,54 +70,18 @@ public class PostAction implements Action {
 			page++;
 		}
 
-		// 해당하는 페이지에 데이터만 주기
-		ArrayList<PostVO> slicedata = new ArrayList<PostVO>();
-		if(datas.size()>0) {
-			for(int i=(index-1)*6; i<index*6; i++) { // 현재 인덱스에 -1*6~현재인덱스*6 까지의 데이터만 넘겨주기
-				PostVO vo = datas.get(i);
-				// vo에 있는 pdate
-				String pdate = vo.getPdate();
-				Date now = new Date();
-				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-				String today = sdf.format(now);
-
-				// 포스팅 날짜와 오늘 날짜 같은 타입으로 변환
-				Date datePdate = null;
-				Date dateToday = null;
-				try {
-					datePdate = sdf.parse(pdate);
-					dateToday = sdf.parse(today);
-				} catch (ParseException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-
-				// 시차 계산
-				long diff = dateToday.getTime() - datePdate.getTime();
-				long timeDif = diff / (60 * 60 * 1000); 
-
-				vo.setNew(false);	// 디폴트 false
-				if(timeDif<24) {	// 현재시간 - pdate 가 24시간 이하면 true
-					vo.setNew(true);
-				}
-
-				// 넘겨줄 날짜 슬라이싱
-				SimpleDateFormat sliceDate = new SimpleDateFormat("yyyy-MM-dd");
-				pdate = sliceDate.format(datePdate);
-				vo.setPdate(pdate);
-				System.out.println("slicedata에 들어가는 data = "+vo);
-				slicedata.add(vo);
-				if(i==datas.size()-1) {
-					break;
-				}
-			}
+		// 해당하는 페이지에 데이터만 주기 캡슐화 진행
+		Date now = new Date();
+		DateSlice ds = new DateSlice(datas, now, index);
+		if(datas.size()!=0) {
+			ds.excuteSlice();
 		}
 		request.setAttribute("category", category);
 		request.setAttribute("isLast", paging.isLast());
 		request.setAttribute("isFirst", paging.isFirst());
 		request.setAttribute("pagingIndex", pagingIndex);
 		request.setAttribute("index", index);
-		request.setAttribute("PostList", slicedata); // PostList로 SelectAll 데이터를 넘김
+		request.setAttribute("PostList", ds.getNewData()); // PostList로 SelectAll 데이터를 넘김
 		//System.out.println("mainActio 에서 보내주는 datas == "+datas);
 		// request.setAttribute("commentsCnt", commentsCnt); // 댓글 수 AL로 넘김 0번 인덱스에 1번 포스트의 댓글 갯수 담겨있음! 댓글수 로직 -> 0927 model column 추가로 삭제
 		System.out.println("showList로 가자 : "+url);
